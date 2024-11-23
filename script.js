3f04d488fb5a102ddb226b07329ad40e15c530cd
vellt <szantobenjamin97@gmail.com>
2024-11-24 00:05:28 +0100

    doga

---
+++ script.js
@@ -0,0 +1,13 @@
+const gomb =document.getElementById('gomb');
+const dolgozat = document.getElementById('dolgozat');
+
+gomb.addEventListener('click',async function(){
+    const data= await fetch("https://yesno.wtf/api");
+    const json= await data.json();
+    console.log(json);
+    
+    gomb.style.display='none';
+
+    dolgozat.textContent=json.answer=="yes"?"NA NÁ!":"MA NE!";
+    document.body.style.backgroundImage = `url('${json.image}')`;
+});
